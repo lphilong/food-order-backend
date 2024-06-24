b0c88eb46d00f52aeaf716eda0fe72d46557d972
Long <lpl141020@gmail.com>
2024-06-24 19:00:07 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,6 @@ mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to database!"));
 
-/*cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-*/
 const app = express();
 
 app.use(cors());
@@ -24,12 +18,9 @@ app.use(express.json());
 app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "health OK!" });
 });
-/*
+
 app.use("/api/my/user", myUserRoute);
-app.use("/api/my/restaurant", myRestaurantRoute);
-app.use("/api/restaurant", restaurantRoute);
-app.use("/api/order", orderRoute);
-*/
+
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
 });
