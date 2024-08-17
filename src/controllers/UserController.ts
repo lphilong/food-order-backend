2055c4953abc9f6af636a86ac6a08ab7c670382d
Long <lpl141020@gmail.com>
2024-08-17 21:04:40 +0700

    update

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import User from "../models/user";
+import Message from "../models/message";
 
 const getCurrentUser = async (req: Request, res: Response) => {
   try {
